273de8ca89070c190ee1a58c49fcf62de42c9bf4
andersonhavah <havahanderson@gmail.com>
2026-02-13 19:30:46 +0000

    auth validation rules

---
+++ middleware/authValidation.js
@@ -0,0 +1,56 @@
+const { body } = require('express-validator');
+
+const registerValidationRules = () => {
+  return [
+    body('email')
+      .trim()
+      .notEmpty()
+      .withMessage('Email is required')
+      .isEmail()
+      .withMessage('Please provide a valid email')
+      .normalizeEmail(),
+    
+    body('password')
+      .notEmpty()
+      .withMessage('Password is required')
+      .isLength({ min: 8 })
+      .withMessage('Password must be at least 8 characters long')
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
+      .withMessage('Password must contain at least one uppercase letter, one lowercase letter, and one number'),
+    
+    body('firstName')
+      .trim()
+      .notEmpty()
+      .withMessage('First name is required')
+      .isLength({ min: 2, max: 50 })
+      .withMessage('First name must be between 2 and 50 characters'),
+    
+    body('lastName')
+      .trim()
+      .notEmpty()
+      .withMessage('Last name is required')
+      .isLength({ min: 2, max: 50 })
+      .withMessage('Last name must be between 2 and 50 characters')
+  ];
+};
+
+const loginValidationRules = () => {
+  return [
+    body('email')
+      .trim()
+      .notEmpty()
+      .withMessage('Email is required')
+      .isEmail()
+      .withMessage('Please provide a valid email')
+      .normalizeEmail(),
+    
+    body('password')
+      .notEmpty()
+      .withMessage('Password is required')
+  ];
+};
+
+module.exports = {
+  registerValidationRules,
+  loginValidationRules
+};
