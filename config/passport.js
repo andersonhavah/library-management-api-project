2f8b2f4e81f99847e2b65a28f57bf10f5e642a3e
andersonhavah <havahanderson@gmail.com>
2026-02-14 13:36:31 +0000

    Added local strategy

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
+const LocalStrategy = require('passport-local').Strategy;
 
 module.exports = function(passport) {
   passport.use(
@@ -47,6 +48,27 @@ module.exports = function(passport) {
         }
       }
     )
+  );
+
+  // --- Add Local Strategy ---
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email }).select('+password');
+        if (!user) {
+          return done(null, false, { message: 'Invalid credentials' });
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+          return done(null, false, { message: 'Invalid credentials' });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    })
   );
 
   // Serialize user for session
